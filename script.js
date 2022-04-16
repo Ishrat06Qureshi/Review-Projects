6dd43750668a8fea7d71134beb0fbb054f3fe146
qureshi <ishrat.qureshi@pwc.com>
2022-04-16 21:23:10 +0500

    changes include : forward and backward functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const REVIEWS_DATA = [
+const REVIEWS_JSON = [
 	{
 		reviewerAvatar: './assets/profileOne.jpg',
 		reviewerName: 'alexa',
@@ -6,7 +6,6 @@ const REVIEWS_DATA = [
 		reviews:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iustoasperiores debitis incidunt, eius earum ipsam cupiditate libero?Iste, doloremque nihil?',
 	},
-
 	{
 		reviewerAvatar: './assets/profileTwo.webp',
 		reviewerName: 'Dorson',
@@ -36,11 +35,38 @@ const REVIEWS_DATA = [
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iustoasperiores debitis incidunt, eius earum ipsam cupiditate libero?Iste, doloremque nihil?',
 	},
 ];
-
-let reviewCounter = 0;
+let counter = 0;
 let forwardButton = document.getElementById('forward-btn');
 let backwardButton = document.getElementById('back-btn');
 let reviewerAvatar = document.getElementById('person-img');
 let reviewerName = document.getElementById('author');
 let reviewerDesignation = document.getElementById('job');
 let reviews = document.getElementById('info');
+
+const setReviews = (reviewId) => {
+	let selectedReview = REVIEWS_JSON[reviewId];
+	reviewerAvatar.src = selectedReview.reviewerAvatar;
+	reviewerName.innerHTML = selectedReview.reviewerName;
+	reviewerDesignation.innerHTML = selectedReview.reviewerDesignation;
+	reviews.innerHTML = selectedReview.reviews;
+};
+const forwardReview = () => {
+	if (counter < REVIEWS_JSON.length - 1) {
+		counter = counter + 1;
+		setReviews(counter);
+	} else {
+		forwardButton.disabled = true;
+	}
+};
+const backReview = () => {
+	if (counter > 0) {
+		counter = counter - 1;
+		setReviews(counter);
+	} else {
+		console.log('counter',counter);
+		backwardButton.disabled = true;
+	}
+};
+console.log(forwardButton);
+forwardButton.addEventListener('click', forwardReview);
+backwardButton.addEventListener('click', backReview);
